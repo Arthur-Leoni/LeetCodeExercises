95c4ced58ac8e97b20500635ad5519a4f40ab30b
arthur-leoni <arthurleoni.al@gmail.com>
2023-03-18 15:10:07 -0300

    correction

--- src/main/java/org/example/leetcode/LongestSubstring/Solution.java
+++ src/main/java/org/example/leetcode/LongestSubstring/Solution.java
@@ -1,25 +1,46 @@
 package org.example.leetcode.LongestSubstring;
 
 public class Solution {
-        public static int lengthOfLongestSubstring(String s) {
-                String longestSubstring = "";
-                String substring = "";
-                boolean containsNewChar = false;
-                char newChar = 0;
+    public static int lengthOfLongestSubstring(String s) {
+        String longestSubstring = "";
+        String actualString = "";
+        boolean containsNewChar = false;
+        char newChar = 0;
 
-               for(int c = 0; c < s.length(); c++ ){
-                       System.out.println(c);
-                       newChar =  s.charAt(c);
-                       containsNewChar = substring.contains(Character.toString(newChar));
-                       substring = substring + newChar;
-                       if (!containsNewChar || c == s.length()-1){
-                          if (substring != null && substring.length() > longestSubstring.length())   {
-                                  longestSubstring = substring;
-                          }
-                       } else {
-                               substring = "";
-                       }
-               }
-               return longestSubstring.length();
+        for(int c = 0; c != s.length(); c++ ){
+            System.out.println(c+" "+ s.charAt(c));
+            newChar =  s.charAt(c);
+            containsNewChar = isContainsNewChar(actualString, newChar);
+            actualString = actualString + newChar;
+            if (!containsNewChar){
+                if (actualString != null && actualString.length() > longestSubstring.length())   {
+                    longestSubstring = actualString;
+                }
+            } else {
+                actualString = cleanString(actualString, newChar);
+            }
         }
+        return longestSubstring.length();
+    }
+
+    private static boolean isContainsNewChar(String substring, char newChar) {
+        return substring.contains(Character.toString(newChar));
+    }
+
+    private static String cleanString(String actualString, char newChar) {
+        while (!containsOnlyOneChar(actualString, newChar) && isContainsNewChar(actualString, newChar) && actualString.length()>1) {
+            actualString = actualString.substring(1);
+        }
+        return actualString;
+    }
+
+    public static boolean containsOnlyOneChar(String substring, char newChar) {
+        int count =0;
+        for (int i = 0; i < substring.length(); i++) {
+            if (substring.charAt(i) == newChar) {
+                count++;
+            }
+        }
+        return (count > 1) ? false : true;
+    }
 }
