448b9862a7b24d582481feb8486e632e87424138
arthur-leoni <arthurleoni.al@gmail.com>
2023-03-24 17:14:19 -0300

    724. Find Pivot Index

--- src/main/java/org/example/leetcode/findPivotIndex724/Main.java
+++ src/main/java/org/example/leetcode/findPivotIndex724/Main.java
@@ -3,5 +3,23 @@ package org.example.leetcode.findPivotIndex724;
 
 public class Main {
     public static void main(String[] args) {
+        //Given an array of integers nums, calculate the pivot index of this array.
+        //
+        //The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+        //
+        //If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+        //
+        //Return the leftmost pivot index. If no such index exists, return -1
+        int[] case1 = {1,7,3,6,5,6};
+        int[] case2 = {1,2,3};
+        int[] case3 = {2,1,-1};
+
+        System.out.print(Solution.pivotIndex(case1));
+        System.out.println();
+        System.out.print(Solution.pivotIndex(case2));
+        System.out.println();
+        System.out.print(Solution.pivotIndex(case3));
+
+
     }
 }
--- src/main/java/org/example/leetcode/findPivotIndex724/Solution.java
+++ src/main/java/org/example/leetcode/findPivotIndex724/Solution.java
@@ -1,9 +1,27 @@
 package org.example.leetcode.findPivotIndex724;
 
 public class Solution {
-        public int pivotIndex(int[] nums) {
+        public static int pivotIndex(int[] nums) {
+                for (int c = 0; c < nums.length; c++) {
+                        int sumLeft = 0;
+                        int sumRight = 0;
+                        int range = 0;
 
-                return 0;
+                        while (c-range >= 0 || c+range <= nums.length) {
+                                if(c-range >= 0) {
+                                        sumLeft = nums[c-range] + sumLeft;
+                                }
+                                if(c+range <= nums.length-1) {
+                                        sumRight = nums[c+range] + sumRight;;
+                                }
+                                range++;
+                        }
+
+                        if(sumLeft == sumRight){
+
+                                return c;
+                        }
+                }
+                return -1;
         }
-
 }
